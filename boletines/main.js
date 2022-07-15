d9122f1e27ef3968736efa5251ac90fdcbddf31a
danijosflo <danjosflo@gmail.com>
2022-07-14 23:44:34 -0500

    intentandolo nuevamente

---
+++ boletines/main.js
@@ -0,0 +1,32 @@
+//Obtener los elementos de la clase .close
+
+let links = document.querySelectorAll(".close");
+
+//Recorrerlos
+
+links.forEach(function(link){
+
+    //Agregar un evento click a cada uno de ellos
+
+    link.addEventListener("click",function(evento){
+        evento.preventDefault();
+        let content = document.querySelector('.content');
+
+        //Quitarle las clases de animacion que ya tiene
+
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        //Agregar clases para animar su salida fadeOutUp
+
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        setTimeout(function(){
+            location.href = "../index.html";
+        },1000);
+        //setInterval se ejecurta cada tiempo que se le indique
+        
+        return false;
+    })
+})
